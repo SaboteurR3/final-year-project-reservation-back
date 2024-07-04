7467fdb7b833301e6f41c99156bcb2b549710c47
Capnsockless <demetre246@outlook.com>
2024-07-05 00:54:37 +0400

    Added UserPreference class

---
+++ src/main/java/ge/project/common/recommend/UserPreference.java
@@ -0,0 +1,30 @@
+package ge.project.common.recommend;
+
+import ge.project.common.recommend.climate.ClimateData;
+import lombok.Getter;
+
+@Getter
+public class UserPreference {
+
+    private ClimateData preferredClimate;
+
+    //wishSimilar determines how similar they want a country to be to their own
+    public UserPreference(ClimateData homeClimate, double wishSimilar){
+        double preferredTemperature = normalize(homeClimate.getAvgTemperature(), wishSimilar,
+                RecommenderGlobals.MIN_TEMPERATURE, RecommenderGlobals.MAX_TEMPERATURE);
+        double preferredHumidity = normalize(homeClimate.getAvgHumidity(), wishSimilar,
+                RecommenderGlobals.MIN_HUMIDITY, RecommenderGlobals.MAX_HUMIDITY);
+        double preferredRainfall = normalize(homeClimate.getAvgRainfall(), wishSimilar,
+                RecommenderGlobals.MIN_RAINFALL, RecommenderGlobals.MAX_RAINFALL);
+
+        preferredClimate = new ClimateData(preferredTemperature, preferredHumidity, preferredRainfall);
+    }
+
+    public static double normalize(double value, double multiplicator, double min, double max) {
+        if (min == max) {
+            throw new IllegalArgumentException("min and max cannot be the same value");
+        }
+        double normalized = ((value - min) / (max - min)) * 2 - 1;
+        return normalized * multiplicator;
+    }
+}
